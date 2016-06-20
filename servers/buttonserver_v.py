a6f65a2548c146627dce1bda609e2eaac260a70a
Minae Kwon <Minae@Minaes-MacBook-Pro.local>
2016-06-20 15:52:53 -0400

    revising buttonserver

--- servers/buttonserver_v.py
+++ servers/buttonserver_v.py
@@ -293,7 +293,7 @@ def handle_survey():
 
   if(cheating == True):
     data[mturk_id].append("INCOMPLETE")
-    with open('output/log.json', 'a') as outfile:
+    with open('output/log-cheating.json', 'a') as outfile:
       json.dump(data, outfile)
     return "<p>It appears that the HIT has not been fully completed. Please complete the HIT again by pasting this link into your browser: http://studies.personalrobotics.ri.cmu.edu/minaek/index.html </p>"
   
@@ -301,7 +301,7 @@ def handle_survey():
   for i in xrange(1,17):
     data[mturk_id].append(request.forms.get(str(i)))
   #data[mturk_id].append(request.forms.get("t3"))
-  with open('output/log.json', 'a') as outfile:
+  with open('output/log-secondbatch.json', 'w') as outfile:
     json.dump(data, outfile)
   print("User {} submitted the survey".format(mturk_id))
   return "<p> Your answers have been submitted. ID for mturk: {}".format(mturk_id)
